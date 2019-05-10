892f58c3de833b35b2c50e135279c6538e7de7a6
yinhe17show <yinhe17show@163.com>
2019-05-10 14:18:19 +0800

    init

---
+++ src/main/java/com/example/JavaDemo/JavaDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.JavaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String Index()
+	{
+		return "Hello World,Java Demo";
+	}
+
+}
